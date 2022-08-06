f662ee8521ba4c580bf7a0b5fa17bbf8a60f605f
ruslooob <rm952576@gmail.com>
2022-08-06 15:42:54 +0300

    delete .idea folder. Add product view and companies sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 6,
+        spaceBetween: 50,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets: 3
+        },
 
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+})
